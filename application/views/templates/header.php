7bf9f3945f8ab6fa0fc87cdb459b7fea140966ae
Ryan Baxley <ryan.baxley1@gmail.com>
2016-06-12 12:11:37 -0400

    first final commit

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,11 +2,10 @@
 <html>
 	<?php
 	if(isset($_ENV["SERVER_NAME"])){
-		$base = $_ENV["SERVER_NAME"];
+		$base = "";
 	}else{
 		$base = base_url();
 	}
-	echo "base is ".$base;
 	?>
     <script type="text/javascript" src="<?php echo $base.'assets/js/Chart.bundle.js'; ?>"></script>
     <script type="text/javascript" src="<?php echo $base.'assets/js/jQuery-1.12.4.min.js'; ?>"></script>
