6178f62bd36ba664a6cedebeba03ae0a9cd7d19a
Ryan Baxley <ryan.baxley1@gmail.com>
2016-07-09 04:58:09 -0400

    Fixed double footer

--- application/views/pages/dummy.php
+++ application/views/pages/dummy.php
@@ -351,12 +351,4 @@
     
     
     </div>
-</div>              <footer class="footer">
-                    <div class="container">
-                        <p class="text-muted text-center">
-                            Ryan Baxley - 2016 - <a href="https://github.com/rynobax/AnalyzeSpotify">Source Code Here</a>
-                        </p>
-                    </div>
-                </footer>
-        </body>
-</html>
+</div>
